0166a16ec6a17015907730085df44a74dddbb11a
iida <iida@cryptogarage.co.jp>
2022-10-25 17:43:23 +0900

    add

--- src/App.js
+++ src/App.js
@@ -15,6 +15,7 @@ import { toHex, truncateAddress } from "./utils";
 import { ethers } from "ethers";
 import Web3Modal from "web3modal";
 import { providerOptions } from "./providerOptions";
+import axios from "axios";
 
 const web3Modal = new Web3Modal({
   cacheProvider: true, // optional
@@ -78,12 +79,27 @@ export default function Home() {
     }
   };
 
+  const api = axios.create({
+    baseURL: "https://auth-api-eight.vercel.app/"
+  });
+
+  const apiGetSignMessage = async () => {
+    const response = await api.get(`/`);
+    const { result } = response;
+    return result;
+  };
+
   const signMessage = async () => {
     if (!library) return;
+
+    let signMessage = await apiGetSignMessage()
+
+    console.log('signMessage = ', signMessage)
+
     try {
       const signature = await library.provider.request({
         method: "personal_sign",
-        params: [message, account]
+        params: [signMessage, account]
       });
       setSignedMessage(message);
       setSignature(signature);
@@ -161,29 +177,6 @@ export default function Home() {
         If you're in the sandbox, first "Open in New Window" ⬆️
       </Text>
       <VStack justifyContent="center" alignItems="center" h="100vh">
-        <HStack marginBottom="10px">
-          <Text
-            margin="0"
-            lineHeight="1.15"
-            fontSize={["1.5em", "2em", "3em", "4em"]}
-            fontWeight="600"
-          >
-            Let's connect with
-          </Text>
-          <Text
-            margin="0"
-            lineHeight="1.15"
-            fontSize={["1.5em", "2em", "3em", "4em"]}
-            fontWeight="600"
-            sx={{
-              background: "linear-gradient(90deg, #1652f0 0%, #b9cbfb 70.35%)",
-              WebkitBackgroundClip: "text",
-              WebkitTextFillColor: "transparent"
-            }}
-          >
-            Web3Modal
-          </Text>
-        </HStack>
         <HStack>
           {!account ? (
             <Button onClick={connectWallet}>Connect Wallet</Button>
@@ -192,19 +185,10 @@ export default function Home() {
           )}
         </HStack>
         <VStack justifyContent="center" alignItems="center" padding="10px 0">
-          <HStack>
-            <Text>{`Connection Status: `}</Text>
-            {account ? (
-              <CheckCircleIcon color="green" />
-            ) : (
-              <WarningIcon color="#cd5700" />
-            )}
-          </HStack>
 
           <Tooltip label={account} placement="right">
-            <Text>{`Account: ${truncateAddress(account)}`}</Text>
+            <Text>{`connect address: ${account}`}</Text>
           </Tooltip>
-          <Text>{`Network ID: ${chainId ? chainId : "No Network"}`}</Text>
         </VStack>
         {account && (
           <HStack justifyContent="flex-start" alignItems="flex-start">
@@ -216,35 +200,9 @@ export default function Home() {
               padding="10px"
             >
               <VStack>
-                <Button onClick={switchNetwork} isDisabled={!network}>
-                  Switch Network
-                </Button>
-                <Select placeholder="Select network" onChange={handleNetwork}>
-                  <option value="3">Ropsten</option>
-                  <option value="4">Rinkeby</option>
-                  <option value="42">Kovan</option>
-                  <option value="1666600000">Harmony</option>
-                  <option value="42220">Celo</option>
-                </Select>
-              </VStack>
-            </Box>
-            <Box
-              maxW="sm"
-              borderWidth="1px"
-              borderRadius="lg"
-              overflow="hidden"
-              padding="10px"
-            >
-              <VStack>
-                <Button onClick={signMessage} isDisabled={!message}>
+                <Button onClick={signMessage}>
                   Sign Message
                 </Button>
-                <Input
-                  placeholder="Set Message"
-                  maxLength={20}
-                  onChange={handleInput}
-                  w="140px"
-                />
                 {signature ? (
                   <Tooltip label={signature} placement="bottom">
                     <Text>{`Signature: ${truncateAddress(signature)}`}</Text>
